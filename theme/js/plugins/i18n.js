64c2c0c8ad6ca1034001c274a8bde89593b5ae66
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-03-10 11:02:14 +0100

    feat: add pt locale

--- theme/js/plugins/i18n.js
+++ theme/js/plugins/i18n.js
@@ -6,13 +6,15 @@ import { createI18n } from "vue-i18n";
 
 //TODO : use a glob when supported in Parcel 2 : https://github.com/parcel-bundler/parcel/issues/4683
 import en from "../locales/en.json";
-import fr from "../locales/fr.json"
+import fr from "../locales/fr.json";
+import pt from "../locales/pt.json";
 
 export default i18n = createI18n({
   locale: config.lang,
   messages: {
     en,
-    fr
+    fr,
+    pt,
   },
   formatFallbackMessages: true
 });
